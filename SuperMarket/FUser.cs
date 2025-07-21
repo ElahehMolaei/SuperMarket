3e27a95ffbd65e5fd8a811ba4fd1919167b91bb9
ElahehMolaei <molaei.ela@gmail.com>
2025-07-21 20:28:40 +0330

    S10

--- SuperMarket/FUser.cs
+++ SuperMarket/FUser.cs
@@ -31,19 +31,31 @@ namespace SuperMarket
                 //create a connection to our interested DB:
                 SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=SuperMarket;Integrated Security=True;Encrypt=False");
                 conn.Open();
-                //insert into user table
-                SqlCommand com = new SqlCommand("INSERT INTO TUser (username, pass, userType) VALUES (@username,@pass,@userType)",conn);
-                com.Parameters.AddWithValue("@username",textBox1.Text);
-                com.Parameters.AddWithValue("@pass", textBox2.Text);
-                com.Parameters.AddWithValue("@userType", radioButton1.Checked?0:1);
-                com.ExecuteNonQuery();
-                conn.Close();
 
-                //after adding a new user , the texboxes should be empty for adding another user
-                textBox1.Text = textBox2.Text = "";
-                radioButton1.Checked = true;
-                textBox1.Focus();
-                MessageBox.Show("کاربر جدید با موفقیت اضافه شد", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // 1.chek tekrari nabudan username ya 2. try catch 
+
+                try
+                {
+                    //insert into user table
+                    SqlCommand com = new SqlCommand("INSERT INTO TUser (username, pass, userType) VALUES (@username,@pass,@userType)", conn);
+                    com.Parameters.AddWithValue("@username", textBox1.Text);
+                    com.Parameters.AddWithValue("@pass", textBox2.Text);
+                    com.Parameters.AddWithValue("@userType", radioButton1.Checked ? 0 : 1);
+                    com.ExecuteNonQuery();
+                    conn.Close();
+
+                    //after adding a new user , the texboxes should be empty for adding another user
+                    textBox1.Text = textBox2.Text = "";
+                    radioButton1.Checked = true;
+                    textBox1.Focus();
+                    MessageBox.Show("کاربر جدید با موفقیت اضافه شد", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex) 
+                {
+                    MessageBox.Show("قبلا این نام کاربری ثبت شده است.", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+
             }
         }
     }
