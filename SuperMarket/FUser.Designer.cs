99c2a3a378ed136f88f3088bfe51e9a076ab0399
ElahehMolaei <molaei.ela@gmail.com>
2025-07-21 21:05:09 +0330

    S11

--- SuperMarket/FUser.Designer.cs
+++ SuperMarket/FUser.Designer.cs
@@ -31,6 +31,7 @@ namespace SuperMarket
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
@@ -40,7 +41,11 @@ namespace SuperMarket
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -65,7 +70,7 @@ namespace SuperMarket
             this.button1.Location = new System.Drawing.Point(56, 183);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(185, 23);
-            this.button1.TabIndex = 7;
+            this.button1.TabIndex = 4;
             this.button1.Text = "ثبت اطلاعات کاربر";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -76,7 +81,7 @@ namespace SuperMarket
             this.radioButton2.Location = new System.Drawing.Point(95, 134);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(87, 17);
-            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabIndex = 3;
             this.radioButton2.Text = "مدیر سیستم";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
@@ -87,7 +92,7 @@ namespace SuperMarket
             this.radioButton1.Location = new System.Drawing.Point(225, 134);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(73, 17);
-            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabIndex = 2;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "کاربر عادی";
             this.radioButton1.UseVisualStyleBackColor = true;
@@ -97,7 +102,7 @@ namespace SuperMarket
             this.textBox2.Location = new System.Drawing.Point(56, 96);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(244, 21);
-            this.textBox2.TabIndex = 4;
+            this.textBox2.TabIndex = 1;
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBox1
@@ -135,10 +140,33 @@ namespace SuperMarket
             this.label1.TabIndex = 0;
             this.label1.Text = "نام کاربری :";
             // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Linen;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 20);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(498, 183);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 246);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(542, 224);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "کاربران سیستم";
+            // 
             // FUser
             // 
             this.AcceptButton = this.button1;
-            this.ClientSize = new System.Drawing.Size(566, 390);
+            this.ClientSize = new System.Drawing.Size(570, 482);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MaximizeBox = false;
@@ -146,8 +174,11 @@ namespace SuperMarket
             this.Name = "FUser";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.Text = "فرم افزودن کاربر جدید";
+            this.Load += new System.EventHandler(this.FUser_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -163,5 +194,7 @@ namespace SuperMarket
         private TextBox textBox2;
         private TextBox textBox1;
         private Button button1;
+        private DataGridView dataGridView1;
+        private GroupBox groupBox2;
     }
 }
--- SuperMarket/FUser.cs
+++ SuperMarket/FUser.cs
@@ -13,50 +13,111 @@ namespace SuperMarket
 {
     public partial class FUser : Form
     {
+        //create a connection to our interested DB:
+        SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=SuperMarket;Integrated Security=True;Encrypt=False");
         public FUser()
         {
             InitializeComponent();
         }
 
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    if(textBox1.Text=="")
+        //        textBox1.Focus();
+        //    else if(textBox2.Text=="")
+        //        textBox2.Focus();
+        //    else
+        //    {
+        //        //sabte etelatae karbari
+
+        //        ////create a connection to our interested DB:
+        //        //SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=SuperMarket;Integrated Security=True;Encrypt=False");
+
+        //        // 1.chek tekrari nabudan username ya 2. try catch 
+
+        //        try
+        //        {
+        //            conn.Open();
+
+        //            //INSERT INTO table
+        //            SqlCommand com = new SqlCommand("INSERT INTO TUser (username, pass, userType) VALUES (@username,@pass,@userType)", conn);
+        //            com.Parameters.AddWithValue("@username", textBox1.Text);
+        //            com.Parameters.AddWithValue("@pass", textBox2.Text);
+        //            com.Parameters.AddWithValue("@userType", radioButton1.Checked ? 0 : 1);
+        //            com.ExecuteNonQuery();
+        //            conn.Close();
+
+        //            //after adding a new user , the texboxes should be empty for adding another user
+        //            textBox1.Text = textBox2.Text = "";
+        //            radioButton1.Checked = true;
+        //            textBox1.Focus();
+        //            MessageBox.Show("کاربر جدید با موفقیت اضافه شد", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //            FUser_Load(sender, e);
+        //        }
+        //        catch (Exception ex) 
+        //        {
+        //            MessageBox.Show("قبلا این نام کاربری ثبت شده است.", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        //        }
+
+        //    }
+        //}
         private void button1_Click(object sender, EventArgs e)
         {
-            if(textBox1.Text=="")
+            if (textBox1.Text == "")
                 textBox1.Focus();
-            else if(textBox2.Text=="")
+            else if (textBox2.Text == "")
                 textBox2.Focus();
             else
             {
-                //sabte etelatae karbari
-
-                //create a connection to our interested DB:
-                SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=SuperMarket;Integrated Security=True;Encrypt=False");
-                conn.Open();
-
-                // 1.chek tekrari nabudan username ya 2. try catch 
-
                 try
                 {
-                    //insert into user table
-                    SqlCommand com = new SqlCommand("INSERT INTO TUser (username, pass, userType) VALUES (@username,@pass,@userType)", conn);
-                    com.Parameters.AddWithValue("@username", textBox1.Text);
-                    com.Parameters.AddWithValue("@pass", textBox2.Text);
-                    com.Parameters.AddWithValue("@userType", radioButton1.Checked ? 0 : 1);
-                    com.ExecuteNonQuery();
-                    conn.Close();
+                    // استفاده از using برای اتصال و دستور
+                    using (SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=SuperMarket;Integrated Security=True;Encrypt=False"))
+                    {
+                        conn.Open();
 
-                    //after adding a new user , the texboxes should be empty for adding another user
-                    textBox1.Text = textBox2.Text = "";
-                    radioButton1.Checked = true;
-                    textBox1.Focus();
-                    MessageBox.Show("کاربر جدید با موفقیت اضافه شد", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        // چک کردن تکراری نبودن کاربر
+                        SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM TUser WHERE username = @username", conn);
+                        checkCmd.Parameters.AddWithValue("@username", textBox1.Text);
+                        int userCount = (int)checkCmd.ExecuteScalar();
+
+                        if (userCount > 0)
+                        {
+                            MessageBox.Show("قبلا این نام کاربری ثبت شده است.", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            return; // خروج از متد اگر کاربر تکراری باشد
+                        }
+
+                        // درج کاربر جدید
+                        SqlCommand insertCmd = new SqlCommand("INSERT INTO TUser (username, pass, userType) VALUES (@username, @pass, @userType)", conn);
+                        insertCmd.Parameters.AddWithValue("@username", textBox1.Text);
+                        insertCmd.Parameters.AddWithValue("@pass", textBox2.Text);
+                        insertCmd.Parameters.AddWithValue("@userType", radioButton1.Checked ? 0 : 1);
+                        insertCmd.ExecuteNonQuery();
+
+                        textBox1.Text = textBox2.Text = "";
+                        radioButton1.Checked = true;
+                        textBox1.Focus();
+                        MessageBox.Show("کاربر جدید با موفقیت اضافه شد", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        FUser_Load(sender, e);
+                    }
                 }
-                catch (Exception ex) 
+                catch (Exception ex)
                 {
-                    MessageBox.Show("قبلا این نام کاربری ثبت شده است.", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
+                    MessageBox.Show($"خطا: {ex.Message}", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-
             }
+        }
+
+
+        // dastoorati ke mikhahim hamzaman ba load safhe ejra beshan 
+        private void FUser_Load(object sender, EventArgs e)
+        {
+            //SELECT
+            SqlDataAdapter da = new SqlDataAdapter("SELECT   username AS [نام کاربری], pass AS [رمز عبور], userType AS [سطح دسترسی]\r\nFROM         TUser", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
         }
     }
 }
